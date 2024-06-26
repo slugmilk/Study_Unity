0cae193d3d0380f7a956a53275b42a9781e22be8
slugmilk <115162335+slugmilk@users.noreply.github.com>
2024-06-26 17:10:18 +0900

    swipe car

---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+
+            float swipeLength = endPos.x - this.startPos.x;
+
+            this.speed = swipeLength / 1000f;
+        }
+
+        transform.Translate(this.speed, 0, 0);
+        this.speed *= 0.86f;
+    }
+}
